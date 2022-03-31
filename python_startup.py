8a2d28bb19df1775a12c27488d07a56659f2c3fa
VFaner <vfaner@nationsbenefits.com>
2022-03-31 10:49:27 -0400

    Use IPython as default interactive shell

--- python_startup.py
+++ python_startup.py
@@ -6,6 +6,15 @@ Python interactive mode configs
 # Run configs
 from pprint import pprint  # auto-import pprint
 import visidata as vd
+import os
+
+try:
+    import IPython
+    os.environ['PYTHONSTARTUP'] = ''  # Prevent running this again
+    IPython.start_ipython()
+    raise SystemExit
+except:
+    print("IPython is not available: falling back to standard prompt")
 
 #===============================================================================
 # Sources
